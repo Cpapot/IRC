4bcc17aca875bdec0e5401ee0970212039ca0aea
42Colins <cprojean@icloud.com>
2024-01-27 15:17:02 +0100

    update privmsg

--- inc/client.hpp
+++ inc/client.hpp
@@ -56,7 +56,7 @@ private:
 
 	bool	modeUser(std::vector<std::string> splitLine);
 	bool	modeChannel(std::vector<std::string> splitLine);
-  bool	privateMessage(std::string message, std::vector<std::string> splitLine, int i);
+	bool	privateMessage(std::string message, std::vector<std::string> splitLine, int i);
 	bool	sendFile(std::string message, std::vector<std::string> splitLine, int i);
 
 public:
--- srcs/commands/privmsg.cpp
+++ srcs/commands/privmsg.cpp
@@ -18,6 +18,7 @@
 
 
 bool	IsChannel(std::string str);
+bool	isDcc(std::string line);
 
 bool	client::privmsg(std::vector<std::string> splitLine)
 {
@@ -48,11 +49,10 @@ bool	client::privmsg(std::vector<std::string> splitLine)
 		return false;
 	}
 	splitLine[2].erase(0, 1);
-	message = ":" + _nickname + " PRIVMSG " + splitLine[1] + " :";
 	for (size_t i = 2; i != splitLine.size(); i++)
 		message += splitLine[i] + SPACE;
 	message += END;
-	_serverPtr->getChannel(splitLine[1])->sendToAllExept(message, _clientSocket);
+	_serverPtr->getChannel(splitLine[1])->sendToAllExept(RPL_CHANMSG(_nickname, splitLine[1], message), _clientSocket);
 	if (DEBUG)
 		printShit("#c PRIVMSG : %s : %s", _username.c_str(), message.c_str());
 	return true;
@@ -62,12 +62,14 @@ bool	client::privateMessage(std::string message, std::vector<std::string> splitL
 {
 	std::string reply;
 	i++;
+	std::cout << splitLine[2] << std::endl;
 	for (size_t y = i; y != splitLine.size(); y++)
 	{
-		if (i == 2 && splitLine[i].find("DCC") == 1)
+		if (y == 2 && isDcc(splitLine[y]))
 		{
+			splitLine[y] = "DCC";
 			sendFile(message, splitLine, i);
-			break;
+			return true;
 		}
 		reply += splitLine[y] + SPACE;
 	}
@@ -76,15 +78,42 @@ bool	client::privateMessage(std::string message, std::vector<std::string> splitL
 	return true;
 }
 
+bool	isDcc(std::string line)
+{
+	int isOkay = 0;
+
+	if (line[2] == 'D')
+		isOkay = 1;
+	if (line[3] == 'C' && isOkay == 1)
+		isOkay = 1;
+	if (line[4] == 'C' && isOkay == 1)
+		return (true);
+	return (false);
+}
+
+std::string		getSize(std::string message)
+{
+	std::string	size;
+	for (int i = 0; i != (int)message.size() - 1; i++)
+		size += message[i];
+	return (size);
+}
+
 bool	client::sendFile(std::string message, std::vector<std::string> splitLine, int i)
 {
-	std::cout << "sendFile" << std::endl;
 	for (size_t y = i; y != splitLine.size(); y++)
 	{
-		message += splitLine[y] + SPACE;
+		std::cout << splitLine[y] << " sagranmere" << std::endl;
+		if ((int)y == i)
+			message += "DCC";
+		else if (y == splitLine.size() - 1)
+			message += getSize(splitLine[y]);
+		else
+			message += splitLine[y];
+		message += SPACE;
 	}
 	message += END;
-	sendToClient(message);
+	_serverPtr->getClient(splitLine[1])->sendToClient(message);
 	if (DEBUG)
 		printShit("#c Trying to send a file");
 	return true;
